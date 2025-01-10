10ef4671574bb668f1fe3bb948aa7b5af33a281f
pedrodruviaro <pedrodruviaro@gmail.com>
2025-01-10 09:00:38 -0300

    feat: add mailhog testing route and .env config

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/email', function () {
     dd("Hello RH Management");
+});
+
+// Testing mailhog
+Route::get('/mailhog', function () {
+    Mail::raw('Hello from Laravel App!', function (Message $message) {
+        $message->to('teste@gmail.com')
+            ->subject('Welcome to RH Management!')
+            ->from('rh@rhmanagement.com');
+    });
+
+    dd("Email sent!");
 });
