6d833e8ca986cdfa3116f5546387d0357faf0c2e
pedrodruviaro <pedrodruviaro@gmail.com>
2025-01-10 09:38:02 -0300

    feat: add models relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class User extends Model
 {
-    //
+    public function detail(): HasOne
+    {
+        return $this->hasOne(UserDetail::class);
+    }
+
+    public function department(): BelongsTo
+    {
+        return $this->belongsTo(Department::class);
+    }
 }
